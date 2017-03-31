5e546f92e639cccfcf645d988bd7712235c64cec
Allyson Aberg <allya7897@gmail.com>
2017-03-31 19:51:14 -0400

    a

--- index.js
+++ index.js
@@ -55,11 +55,11 @@ app.post('/webhook/', function (req, res) {
 
 		    }
 		    //SEARCH
-		    if (text === 'Stories') {
+		    if (event.message.text === 'Stories') {
 
 		    	sendTextMessage(sender, "What's your keyword?")
 
-		    	if (text != 'Stories') {
+		    	if (text !== 'Stories') {
 		    	youTube.search('creepypasta', 15, function(error, result) {
   				if (error) {
     				console.log(error);
