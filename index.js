37192ec8b718d45761b787ab5e620ac22fef4078
Allyson Aberg <allya7897@gmail.com>
2017-03-30 20:01:24 -0400

    pls work

--- index.js
+++ index.js
@@ -107,14 +107,22 @@ function sendQuickReply(sender) {
     	{ content_type: 'text', title: 'green', payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN' }
   	]
 	}	
-			}, function(error, response, body) {
+			}
+			    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
 		if (error) {
 		    console.log('Error sending messages: ', error)
 		} else if (response.body.error) {
 		    console.log('Error: ', response.body.error)
 	    }
     })
-
 		}
 function sendGenericMessage(sender, titles, subtitles, images, urls, i) {
     let messageData = {
