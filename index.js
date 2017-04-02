00ceca7a0f81cbd5de9f8ec21c1aa4ab6be5d2f4
Allyson Aberg <allya7897@gmail.com>
2017-04-02 10:28:44 -0400

    more info

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ app.post( '/webhook/', function( req, res ) {
 			}
 
 			if (text === 'Favourites') {
-				sendTextMessage(sender, "Favourites: " + savedDictionary[sender])
+				sendTextMessage(sender, "Favourites: " + savedDictionary[sender].givenTitle)
 			}
 
 
