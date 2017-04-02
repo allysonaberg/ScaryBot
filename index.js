337c632a4a945302945ceff66a0d5a2706995d05
Allyson Aberg <allya7897@gmail.com>
2017-04-02 15:12:43 -0400

    a gi

--- index.js
+++ index.js
@@ -239,7 +239,6 @@ app.post( '/webhook/', function( req, res ) {
 				// saverVideo.push(images[0])
 				// saverVideo.push(urls[0])
 				savedVideo[givenTitle] = titles[0]
-				console.log("HERE: " + savedVideo.givenTitle)
 				savedVideo.subtitle = subtitles[0]
 				savedVideo.image = images[0]
 				savedVideo.url = urls[0]
