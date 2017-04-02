892f9fb3245caa571caadfcd7d7adbd58e903fec
Allyson Aberg <allya7897@gmail.com>
2017-04-02 19:46:46 -0400

    payload

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ app.post( '/webhook/', function( req, res ) {
 			}
 
 			//SAVE TO FAVOURITES
-			if (event.postback.payload.includes("MessageSave-")) {
+			if (event.postback && event.postback.payload.includes("MessageSave-")) {
 				console.log("TEXT: " + text)
 				var saverVideo = []
 				saverVideo.push(titles[0])
