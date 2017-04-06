79ceb8d1be842b9f671ab39d88735b49a36996be
Allyson Aberg <allya7897@gmail.com>
2017-04-06 17:50:12 -0400

    a

--- index.js
+++ index.js
@@ -395,7 +395,7 @@ function storyElement(xy, result, titles, subtitles, images, urls) {
 function genericMessageTemplateSaved( sender, savedDictionary) {
 	var elements = []
 	console.log("OUTSIDE with: " + savedDictionary[sender].length)
-	console.log("DICTIONARY: " + savedDictionary[sender][3])
+	console.log("DICTIONARY: " + savedDictionary[sender][2])
 	
 	for (var xy = 0; xy < (savedDictionary[sender].length / 4); xy++) {
 		console.log("XY IS: " + xy)
