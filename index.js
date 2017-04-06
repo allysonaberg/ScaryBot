5c2596412c47d61ec4bc071b262d3f9d440c9fe1
Allyson <allyson@Admins-MacBook-Pro-97.local>
2017-04-06 09:58:11 -0400

    missing the d lmao

--- index.js
+++ index.js
@@ -570,7 +570,7 @@ function storyElementSaved(xy, sender, savedDictionary) {
             {
                 type: "postback",
                 title: "Remove",
-                payload: "SaveRemove" + xy
+                payload: "SavedRemove" + xy
             }
         )
   
