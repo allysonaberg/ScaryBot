523260d27c93cef68e838f2f3cead8f242ea5920
Allyson Aberg <allya7897@gmail.com>
2017-04-08 14:18:31 -0400

    a

--- index.js
+++ index.js
@@ -21,26 +21,6 @@ var urls = []
 var CronJob = require( 'cron' ).CronJob;
 
 /* DATABASE */
-var pg = require("pg")
-var http = require("http")
-var port = 5433;
-var host = '127.0.0.1';
-http.createServer(function(req, res) {
-if(req.method == 'POST') {
-insert_records(req,res);
-}
-else if(req.method == 'GET') {
-list_records(req,res);
-}
-else if(req.method == 'PUT') {
-update_record(req,res);
-}
-else if(req.method == 'DELETE') {
-delete_record(req,res);
-}
-}).listen(port,host);
-console.log("Connected to " + port + "   " + host);
-
 /* DATABASE */
 
 var inStories = false
@@ -90,12 +70,11 @@ app.post( '/webhook/', function( req, res ) {
 				let genericGreeting = 'Hi, my name is Scary Bot. I am your personalized creepyPasta scout!'
 				templates.sendTextMessage( sender, genericGreeting )
 				setTimeout( function() {
-					templates.sendQuickReplyMenu( sender, prompt1, option1, option2, option3 )
+					templates.sendQuickReply( sender, prompt1, option1, option2)
 				}, 1000 )
 				let prompt1 = 'What would you like to do?'
 				let option1 = 'Stories'
 				let option2 = 'Subscribe'
-				let option3 = 'Favourites'
 			}
 
 			//SEARCH - OPENING
@@ -176,78 +155,78 @@ app.post( '/webhook/', function( req, res ) {
 					//templates.sendMoreMessage(sender, keyword)
 
 			}
-			//SUBSCRIBE
-			if ( text === 'Subscribe' ) {
+			// //SUBSCRIBE
+			// if ( text === 'Subscribe' ) {
 
-				if ( !isSubscribed ) {
-					inSubscribe = true
-					let message1 = "You can subscribe to daily videos here!"
-					let message2 = "You are currently unsubscribed, would you like to be subscribed?"
-					let option1 = "Yes"
-					let option2 = "No"
-					templates.sendTextMessage( sender, message1 )
-					setTimeout( function() {
-						templates.sendQuickReply( sender, message2, option1, option2 )
-					}, 1000 )
+			// 	if ( !isSubscribed ) {
+			// 		inSubscribe = true
+			// 		let message1 = "You can subscribe to daily videos here!"
+			// 		let message2 = "You are currently unsubscribed, would you like to be subscribed?"
+			// 		let option1 = "Yes"
+			// 		let option2 = "No"
+			// 		templates.sendTextMessage( sender, message1 )
+			// 		setTimeout( function() {
+			// 			templates.sendQuickReply( sender, message2, option1, option2 )
+			// 		}, 1000 )
 
-				} else {
-					let message1 = "You are already subscribed to daily videos, would you like to unsubscribe?"
-					let option2 = "Stay subscribed"
-					let option1 = "Unsubscribe"
-					templates.sendQuickReply( sender, message1, option1, option2 )
+			// 	} else {
+			// 		let message1 = "You are already subscribed to daily videos, would you like to unsubscribe?"
+			// 		let option2 = "Stay subscribed"
+			// 		let option1 = "Unsubscribe"
+			// 		templates.sendQuickReply( sender, message1, option1, option2 )
 
-				}
-			}
+			// 	}
+			// }
 			//SUBSCRIBE TIME
-			if ( text === 'Yes' && inSubscribe && !isSubscribed ) {
-				isSubscribed = true
-				sendTextMessage( sender, "You are now subscribed" )
-				var CronJob = require( 'cron' ).CronJob;
-				var job = new CronJob( {
-					cronTime: '00 00 9 * * 1-7',
-					onTick: function() {
-						/*
-						 * Runs every weekday (Monday through Friday)
-						 * at 11:30:00 AM. It does not run on Saturday
-						 * or Sunday.
-						 */
-						sendTextMessage( sender, "Your daily scary story!" )
-						var random = Math.floor( math.random( ( randomList.length - 1 ) ) )
+			// if ( text === 'Yes' && inSubscribe && !isSubscribed ) {
+			// 	isSubscribed = true
+			// 	sendTextMessage( sender, "You are now subscribed" )
+			// 	var CronJob = require( 'cron' ).CronJob;
+			// 	var job = new CronJob( {
+			// 		cronTime: '00 00 9 * * 1-7',
+			// 		onTick: function() {
+			// 			/*
+			// 			 * Runs every weekday (Monday through Friday)
+			// 			 * at 11:30:00 AM. It does not run on Saturday
+			// 			 * or Sunday.
+			// 			 */
+			// 			sendTextMessage( sender, "Your daily scary story!" )
+			// 			var random = Math.floor( math.random( ( randomList.length - 1 ) ) )
 
-						channelRandomizer()
-						youTube.search( randomList[ random ], 10, function( error, result ) {
-							if ( error ) {
-								console.log( error );
-							} else {
-								console.log( JSON.stringify( result, null, 2 ) );
-								var message = ""
-								var titles = []
-								var subtitles = []
-								var images = []
-								var urls = []
-								for ( var i = 0; i < result.items.length; i++ ) {
-									if ( result.items[ i ].id.kind != "youtube#channel" ) {
-										var title = result.items[ i ].snippet.title.replace( 'Creepypasta', '' )
-										title.replace( '"', '' )
-										titles.push( title )
-										subtitles.push( result.items[ i ].snippet.description )
-										images.push( result.items[ i ].snippet.thumbnails.high.url )
-										urls.push( "https://www.youtube.com/watch?v=" + result.items[ i ].id.videoId )
+			// 			channelRandomizer()
+			// 			youTube.search( randomList[ random ], 10, function( error, result ) {
+			// 				if ( error ) {
+			// 					console.log( error );
+			// 				} else {
+			// 					console.log( JSON.stringify( result, null, 2 ) );
+			// 					var message = ""
+			// 					var titles = []
+			// 					var subtitles = []
+			// 					var images = []
+			// 					var urls = []
+			// 					for ( var i = 0; i < result.items.length; i++ ) {
+			// 						if ( result.items[ i ].id.kind != "youtube#channel" ) {
+			// 							var title = result.items[ i ].snippet.title.replace( 'Creepypasta', '' )
+			// 							title.replace( '"', '' )
+			// 							titles.push( title )
+			// 							subtitles.push( result.items[ i ].snippet.description )
+			// 							images.push( result.items[ i ].snippet.thumbnails.high.url )
+			// 							urls.push( "https://www.youtube.com/watch?v=" + result.items[ i ].id.videoId )
 
-									}
-								}
+			// 						}
+			// 					}
 
-								templates.sendGenericMessageTemplate( sender, result, titles, subtitles, images, urls )
+			// 					templates.sendGenericMessageTemplate( sender, result, titles, subtitles, images, urls )
 
-								inStories = false
-							}
-						} )
-					},
-					start: false,
-					timeZone: 'America/Toronto'
-				} );
-				job.start();
-			}
+			// 					inStories = false
+			// 				}
+			// 			} )
+			// 		},
+			// 		start: false,
+			// 		timeZone: 'America/Toronto'
+			// 	} );
+			// 	job.start();
+			// }
 
 			if ( text === 'Favourites' ) {
 				if ( savedDictionary[ sender ] != undefined && savedDictionary[ sender ].length > 0 ) {
