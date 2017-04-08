b30f1bea389a55edaf42d2a8a718770bd2c43cb5
Allyson Aberg <allya7897@gmail.com>
2017-04-08 11:17:22 -0400

    more nodesqlg

--- index.js
+++ index.js
@@ -23,62 +23,66 @@ var CronJob = require( 'cron' ).CronJob;
 /* DATABASE */
 const pg = require('pg')
 const conString = process.env.SQL_ACCESS_TOKEN
+const client = new pg.Client(conString)
+client.connect()
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
 
+// pg.connect(conString, function (err, client, done) {  
+//   if (err) {
+//     return console.error('error fetching client from pool', err)
+//   }
+//   client.query('SELECT $1::varchar AS my_first_query', ['node hero'], function (err, result) {
+//     done()
 
-pg.connect(conString, function (err, client, done) {  
-  if (err) {
-    return console.error('error fetching client from pool', err)
-  }
-  client.query('SELECT $1::varchar AS my_first_query', ['node hero'], function (err, result) {
-    done()
+//     if (err) {
+//       return console.error('error happened during query', err)
+//     }
+//     console.log(result.rows[0])
+//     process.exit(0)
+//   })
+// })
 
-    if (err) {
-      return console.error('error happened during query', err)
-    }
-    console.log(result.rows[0])
-    process.exit(0)
-  })
-})
+// app.post('/users', function (req, res, next) {  
+//   const user = req.body
 
-app.post('/users', function (req, res, next) {  
-  const user = req.body
+//   pg.connect(conString, function (err, client, done) {
+//     if (err) {
+//       // pass the error to the express error handler
+//       return next(err)
+//     }
+//     client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age], function (err, result) {
+//       done() //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
 
-  pg.connect(conString, function (err, client, done) {
-    if (err) {
-      // pass the error to the express error handler
-      return next(err)
-    }
-    client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age], function (err, result) {
-      done() //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
+//       if (err) {
+//         // pass the error to the express error handler
+//         return next(err)
+//       }
 
-      if (err) {
-        // pass the error to the express error handler
-        return next(err)
-      }
+//       res.send(200)
+//     })
+//   })
+// })
 
-      res.send(200)
-    })
-  })
-})
+// app.get('/users', function (req, res, next) {  
+//   pg.connect(conString, function (err, client, done) {
+//     if (err) {
+//       // pass the error to the express error handler
+//       return next(err)
+//     }
+//     client.query('SELECT name, age FROM users;', [], function (err, result) {
+//       done()
 
-app.get('/users', function (req, res, next) {  
-  pg.connect(conString, function (err, client, done) {
-    if (err) {
-      // pass the error to the express error handler
-      return next(err)
-    }
-    client.query('SELECT name, age FROM users;', [], function (err, result) {
-      done()
+//       if (err) {
+//         // pass the error to the express error handler
+//         return next(err)
+//       }
 
-      if (err) {
-        // pass the error to the express error handler
-        return next(err)
-      }
-
-      res.json(result.rows)
-    })
-  })
-})
+//       res.json(result.rows)
+//     })
+//   })
+// })
 
 /* DATABASE */
 
