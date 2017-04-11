07eb4461d1b02b84e3e71e787194d09235b9d0ff
Allyson Aberg <allya7897@gmail.com>
2017-04-11 18:23:03 -0400

    pls

--- index.js
+++ index.js
@@ -28,24 +28,24 @@ var Schema = mongoose.Schema
 var url = process.env.MONGOLAB_URI
 mongoose.connect(url)
 
-var favouritesSchema = new Schema({
+var favouritesSchema = new Schema({[
+	sender: String
 	meta: [{
-		sender: String
 		title: String,
 		subtitle: String,
 		image: String,
 		url: String
 	}]
-})
+]})
 
 var Favourites = mongoose.model('Favourites', favouritesSchema)
 
 module.exports = Favourites
 
 function dbPopulate(sender, title, subtitle, image, url) {
-	var user = Favourites({ 
+	var user = Favourites({ [
+		sender: "sender",
 		meta:[{
-			sender: "sender"
 			title: "title",
 			subtitle: "subtitle",
 			image: "image",
