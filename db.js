1210800a387d9c87244f7e4fd30fe6faf68edddb
Allyson Aberg <allya7897@gmail.com>
2017-04-13 19:47:19 -0400

    a

--- db.js
+++ db.js
@@ -1,100 +1,100 @@
-'use strict'
+// 'use strict'
 
-var savedVideo = []
-var titles = []
-var subtitles = []
-var images = []
-var urls = []
+// var savedVideo = []
+// var titles = []
+// var subtitles = []
+// var images = []
+// var urls = []
 
-const templates = require( "./templates" )
-const db = require("./db")
-const express = require( 'express' )
-const bodyParser = require( 'body-parser' )
-const request = require( 'request' )
-const app = express()
-const math = require( 'mathjs' )
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
-var url = process.env.MONGOLAB_URI
-mongoose.connect(url)
+// const templates = require( "./templates" )
+// const db = require("./db")
+// const express = require( 'express' )
+// const bodyParser = require( 'body-parser' )
+// const request = require( 'request' )
+// const app = express()
+// const math = require( 'mathjs' )
+// var mongoose = require('mongoose')
+// var Schema = mongoose.Schema
+// var url = process.env.MONGOLAB_URI
+// mongoose.connect(url)
 
-var favouritesSchema = new Schema({
-	meta: [{
-		sender: String,
-		title: String,
-		subtitle: String,
-		image: String,
-		url: String
-	}]
-})
+// var favouritesSchema = new Schema({
+// 	meta: [{
+// 		sender: String,
+// 		title: String,
+// 		subtitle: String,
+// 		image: String,
+// 		url: String
+// 	}]
+// })
 
-var Favourites = mongoose.model('Favourites', favouritesSchema)
+// var Favourites = mongoose.model('Favourites', favouritesSchema)
 
-module.exports = Favourites
+// module.exports = Favourites
 
-//CREATE
-function dbPopulate(sender, title, subtitle, image, url) {
-	var user = Favourites({
-		meta:[{
-			sender: sender,
-			title: title,
-			subtitle: subtitle,
-			image: image,
-			url: url
-		}]
-	})
+// //CREATE
+// function dbPopulate(sender, title, subtitle, image, url) {
+// 	var user = Favourites({
+// 		meta:[{
+// 			sender: sender,
+// 			title: title,
+// 			subtitle: subtitle,
+// 			image: image,
+// 			url: url
+// 		}]
+// 	})
 
-	user.save(function(err) {
-	if (err) console.log("ERROR:" + err)
-		console.log("ADDED IN!!!")
-	})
-}
-
-
-//READ ALL
-function dbList(sender, titles, subtitles, images, urls) {
-	Favourites.find(/*{sender: sender},*/ function(err, favourites) {
-		if (err) throw err
-			//console.log( JSON.stringify( favourites, null, 1) );
-			for (var index = 0; index < favourites.length; index++) {
-					titles.push(favourites[index].meta[0].title)
-					subtitles.push(favourites[index].meta[0].subtitle)
-					images.push(favourites[index].meta[0].image)
-					urls.push(favourites[index].meta[0].url)
-			 }
-
-			 if (titles.length > 0) {
-				templates.sendGenericMessageTemplateSaved(sender, titles, subtitles, images, urls)
-			 }
-	})
-}
-
-//REMOVE
-function dbRemove(sender, title) {
-	Favourites.findOneAndRemove({sender: sender}, {title: title}, function(err) {
-		if (err) throw err
-		console.log("deleted")
-	})
-}
-
-//FIND AND UPDATE (in this case, find by title)
-function dbFindAndUpdate() {
-	Favourites.findById({title: ''}, {title: 'newtitle'}, function(err, user) {
-		if (err) throw err
-
-		console.log("found and updated")
-
-	})
-}
+// 	user.save(function(err) {
+// 	if (err) console.log("ERROR:" + err)
+// 		console.log("ADDED IN!!!")
+// 	})
+// }
 
 
-module.exports = {
-	dbPopulate: dbPopulate,
-	dbList: dbList,
-	dbRemove: dbRemove,
-	dbFindAndUpdate: dbFindAndUpdate,
-	titles: titles,
-	subtitles: subtitles,
-	images: images,
-	urls: urls
-}
+// //READ ALL
+// function dbList(sender, titles, subtitles, images, urls) {
+// 	Favourites.find({sender: sender}, function(err, favourites) {
+// 		if (err) throw err
+// 			//console.log( JSON.stringify( favourites, null, 1) );
+// 			for (var index = 0; index < favourites.length; index++) {
+// 					titles.push(favourites[index].meta[0].title)
+// 					subtitles.push(favourites[index].meta[0].subtitle)
+// 					images.push(favourites[index].meta[0].image)
+// 					urls.push(favourites[index].meta[0].url)
+// 			 }
+
+// 			 if (titles.length > 0) {
+// 				templates.sendGenericMessageTemplateSaved(sender, titles, subtitles, images, urls)
+// 			 }
+// 	})
+// }
+
+// //REMOVE
+// function dbRemove(sender, title) {
+// 	Favourites.findOneAndRemove({sender: sender}, {title: title}, function(err) {
+// 		if (err) throw err
+// 		console.log("deleted")
+// 	})
+// }
+
+// //FIND AND UPDATE (in this case, find by title)
+// function dbFindAndUpdate() {
+// 	Favourites.findById({title: ''}, {title: 'newtitle'}, function(err, user) {
+// 		if (err) throw err
+
+// 		console.log("found and updated")
+
+// 	})
+// }
+
+
+// module.exports = {
+// 	dbPopulate: dbPopulate,
+// 	dbList: dbList,
+// 	dbRemove: dbRemove,
+// 	dbFindAndUpdate: dbFindAndUpdate,
+// 	titles: titles,
+// 	subtitles: subtitles,
+// 	images: images,
+// 	urls: urls
+// }
