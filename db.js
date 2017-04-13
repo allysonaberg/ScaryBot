52d4aaff0a09eb09d5514d4bf825872ed03f1873
Allyson Aberg <allya7897@gmail.com>
2017-04-12 20:34:28 -0400

    index

--- db.js
+++ db.js
@@ -45,10 +45,10 @@ function dbList(sender, titles, subtitles, images, urls) {
 		if (err) throw err
 			console.log( JSON.stringify( favourites, null, 1) );
 			for (var index = 0; index < favourites.length; index++) {
-					titles.push(favourites[i].meta[0].title)
-					subtitles.push(favourites[i].meta[0].subtitle)
-					images.push(favourites[i].meta[0].image)
-					urls.push(favourites[i].meta[0].url)
+					titles.push(favourites[index].meta[0].title)
+					subtitles.push(favourites[index].meta[0].subtitle)
+					images.push(favourites[index].meta[0].image)
+					urls.push(favourites[index].meta[0].url)
 			 }
 	})
 }
