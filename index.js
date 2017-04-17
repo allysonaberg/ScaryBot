aea114da3e85db1a6253cce5c0b1310ac62b7a37
Allyson Aberg <allya7897@gmail.com>
2017-04-17 18:45:28 -0400

    bruh

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.post( '/webhook/', function( req, res ) {
 			}
 
 			if ( text === 'Favourites' ) {
-				templates.dbList sender, titles, subtitles, images, urls, ids)
+				templates.dbList(sender, titles, subtitles, images, urls, ids)
 			}
 
 
