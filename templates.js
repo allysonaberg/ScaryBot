4e8c0df36fd62ecea49d00ab0fca5aff76e3e092
Allyson <allyson@Admins-MacBook-Pro-97.local>
2017-04-18 15:08:22 -0400

    done sender test

--- templates.js
+++ templates.js
@@ -264,7 +264,7 @@ function dbPopulate( sender, title, subtitle, image, url ) {
 		if ( titles.length < 5 ) {
 			var user = Favourites( {
 				meta: [ {
-					sender: '1',
+					sender: sender,
 					title: title,
 					subtitle: subtitle,
 					image: image,
