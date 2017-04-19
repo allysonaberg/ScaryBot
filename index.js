04925268d4c134b16376d390eb5d49eedddfa9ab
Allyson Aberg <allya7897@gmail.com>
2017-04-19 18:39:19 -0400

    Testing result

--- index.js
+++ index.js
@@ -209,6 +209,7 @@ app.post( '/webhook/', function( req, res ) {
     				templates.sendErrorMessage(sender)
   				}
   				else {
+  					console.log("result")
     				var title = result.items[ 0 ].snippet.title.replace( 'Creepypasta', '' )
 					title.replace( '"', '' )
 					titles.push( title )
