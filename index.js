39d2b94be4ab969bfbdff203352caf05e9c6a017
Allyson Aberg <allya7897@gmail.com>
2017-04-18 21:18:57 -0400

    is 5 sec enouch

--- index.js
+++ index.js
@@ -200,7 +200,7 @@ app.post( '/webhook/', function( req, res ) {
 				setTimeout( function() {
 					templates.dbPopulate( sender, title, result.items[ 0 ].snippet.description, result.items[ 0 ].snippet.thumbnails.high.url, "https://www.youtube.com/watch?v=" + result.items[ 0 ].id.videoId)
 					clearArrays(sender, titles, subtitles, images, urls, ids)
-				}, 10000)
+				}, 5000)
   				}
 				})
 				
