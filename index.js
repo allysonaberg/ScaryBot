85c5c601d86489911dc0376f3920c71c21650dee
Allyson <allyson@Admins-MacBook-Pro-97.local>
2017-04-19 11:01:26 -0400

    A

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app.post( '/webhook/', function( req, res ) {
 		let event = req.body.entry[ 0 ].messaging[ i ]
 		let sender = event.sender.id
 		if ( event.message && event.message.text ) {
+			console.log("A TEXT: " + text)
 			let text = event.message.text
 
 			//GREETING
