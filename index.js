d3aec07869419230cf11a0cd5b6703a8545463b8
Allyson Aberg <allya7897@gmail.com>
2017-04-18 21:43:20 -0400

    Added in default message

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ app.post( '/webhook/', function( req, res ) {
 			let defaultMessage2 = "What would you like to do?"
 			let option1 = "Stories"
 			let option2 = "Favourites"
-			templates.sendTextMessage(Sender, defaultMessage1)
+			templates.sendTextMessage(sender, defaultMessage1)
 			setTimeout( function() {
 				templates.sendQuickReply(sender, defaultMessage2, option1, option2)
 			}, 1000)
