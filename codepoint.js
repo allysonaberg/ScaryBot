4eb513cf55e482c251c6b2cbd5029116cf37b4c6
Allyson <allyson@Admins-MacBook-Pro-97.local>
2017-04-19 11:26:34 -0400

    pumpkin added

--- codepoint.js
+++ codepoint.js
@@ -1,8 +1,10 @@
 'use strict'
 var happy = '\uD83D\uDE0A'
 var ghost = '\uD83D\uDC7B'
+var pumpkin = '\uD83C\uDF83'
 
 module.exports = {
 	happy: happy,
-	ghost: ghost
+	ghost: ghost,
+	pumpkin: pumpkin
 }
